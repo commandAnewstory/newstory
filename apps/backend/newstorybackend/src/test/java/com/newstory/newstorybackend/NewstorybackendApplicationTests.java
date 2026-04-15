c3a4cfd0ac840de0c4542f919584ca570dc5d5fa
hyj090915 <hyj090915@gmail.com>
2026-04-15 17:32:23 +0900

    feat: add backend project (#3)

---
+++ apps/backend/newstorybackend/src/test/java/com/newstory/newstorybackend/NewstorybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstory.newstorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstorybackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
