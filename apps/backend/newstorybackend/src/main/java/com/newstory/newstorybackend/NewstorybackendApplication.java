d68bf3bd4200a55d1a61630c12b54b9c5eca5831
hyj090915 <hyj090915@gmail.com>
2026-04-15 17:45:04 +0900

    feat: add backend projectdd

--- apps/backend/newstorybackend/src/main/java/com/newstory/newstorybackend/NewstorybackendApplication.java
+++ apps/backend/newstorybackend/src/main/java/com/newstory/newstorybackend/NewstorybackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewstorybackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NewstorybackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NewstorybackendApplication.class, args);
+	}
 
 }
